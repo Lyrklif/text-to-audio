0e685a768af33c7fd711dac443e15374e3386855
Alexandr Kochurov <b2b4ly@gmail.com>
2024-10-16 09:26:28 +0400

    feat: change default text

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-//in your `main.js` file
 import '../node_modules/flowbite-vue/dist/index.css'
 import './assets/style.css'
-//in your `main.js` file
 
 const app = createApp(App)
 
