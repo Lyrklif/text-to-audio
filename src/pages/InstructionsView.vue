b93f154db484bfb8d3a25b254012f84f24d8888d
Alexandr Kochurov <b2b4ly@gmail.com>
2024-10-16 06:39:00 +0400

    feat: delete links

--- src/pages/InstructionsView.vue
+++ src/pages/InstructionsView.vue
@@ -1,44 +1,20 @@
 <script setup lang="ts">
-import { FwbA, FwbAlert, FwbCard, FwbHeading, FwbP } from 'flowbite-vue'
+import { FwbA, FwbAlert, FwbCard, FwbP } from 'flowbite-vue'
 </script>
 
 <template>
   <main>
-    <FwbCard variant="image" class="p-3 text-center mx-auto">
+    <FwbCard class="text-center mx-auto">
       <RouterLink
         :to="{ name: 'home' }"
-        class="hover:underline text-md font-medium text-gray-500 dark:text-gray-400 mx-auto"
+        class="block w-full text-md font-medium text-gray-500 dark:text-gray-400 mx-auto p-3"
       >
-        Return to main page
+        Home
       </RouterLink>
     </FwbCard>
 
-    <FwbCard variant="image" class="mx-auto mt-10 p-5">
-      <FwbHeading tag="h2" class="mb-6">How to add more voices</FwbHeading>
-
-      <FwbAlert class="text-md mt-3">
-        <FwbA
-          href="https://support.microsoft.com/en-us/topic/download-languages-and-voices-for-immersive-reader-read-mode-and-read-aloud-4c83a8d8-7486-42f7-8e46-2b0fdf753130"
-          class="font-bold"
-          target="_blank"
-        >
-          External link
-        </FwbA>
-        <FwbP>Install a new Text-to-Speech language in Windows 11</FwbP>
-      </FwbAlert>
-
+    <FwbCard variant="image" class="mx-auto mt-10">
       <FwbAlert class="text-md">
-        <FwbA
-          href="https://texttospeech.io/how-to-add-more-voices-for-speech-synthesis-reader-p10"
-          class="font-bold"
-          target="_blank"
-        >
-          External link
-        </FwbA>
-        <FwbP>Install a new Text-to-Speech language in Windows 10</FwbP>
-      </FwbAlert>
-
-      <FwbAlert class="text-md mt-3">
         <FwbA
           href="https://www.avs4you.com/blog/change-text-to-speech-language-voice/"
           class="font-bold"
@@ -46,7 +22,7 @@ import { FwbA, FwbAlert, FwbCard, FwbHeading, FwbP } from 'flowbite-vue'
         >
           External link
         </FwbA>
-        <FwbP>Install a new Text-to-Speech language in Windows 7</FwbP>
+        <FwbP>Install a new Text-to-Speech language on Windows 7, 10 and 11</FwbP>
       </FwbAlert>
     </FwbCard>
   </main>
