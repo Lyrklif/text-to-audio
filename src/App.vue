824dc86519f6e5c9198b0abfdeb80576192841fa
Alexandr Kochurov <b2b4ly@gmail.com>
2024-10-16 08:19:52 +0400

    feat: set grid layout

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,46 @@ import { FwbA, FwbCard, FwbFooter, FwbHeading } from 'flowbite-vue'
 </script>
 
 <template>
-  <div class="min-h-full flex flex-col gap-10 pt-10">
-    <FwbCard variant="image" class="mx-auto p-5">
+  <div class="app gap-10 pt-10">
+    <FwbCard variant="image" class="app-header mx-auto p-5">
       <FwbHeading tag="h1" class="text-center">Text to audio</FwbHeading>
     </FwbCard>
 
-    <RouterView />
+    <main class="app-content">
+      <RouterView />
+    </main>
 
-    <FwbFooter class="mt-auto mb-0">
-      <FwbA href="https://github.com/Lyrklif/text-to-audio" class="ml-auto" target="_blank">
+    <FwbFooter class="app-footer text-center justify-center">
+      <FwbA href="https://github.com/Lyrklif/text-to-audio" target="_blank" class="mx-auto">
         GitHub
       </FwbA>
     </FwbFooter>
   </div>
 </template>
+
+<style>
+.app {
+  min-height: 100vh;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas:
+    'header'
+    'main'
+    'footer';
+}
+
+.app-header {
+  grid-area: header;
+}
+
+.app-content {
+  grid-area: main;
+  padding: 15px 5px 10px 5px;
+}
+
+.app-footer {
+  grid-area: footer;
+}
+</style>
--- src/pages/InstructionsView.vue
+++ src/pages/InstructionsView.vue
@@ -3,27 +3,25 @@ import { FwbA, FwbAlert, FwbCard, FwbP } from 'flowbite-vue'
 </script>
 
 <template>
-  <main>
-    <FwbCard class="text-center mx-auto">
-      <RouterLink
-        :to="{ name: 'home' }"
-        class="block w-full text-md font-medium text-gray-500 dark:text-gray-400 mx-auto p-3"
-      >
-        Home
-      </RouterLink>
-    </FwbCard>
+  <FwbCard class="text-center mx-auto">
+    <RouterLink
+      :to="{ name: 'home' }"
+      class="block w-full text-md font-medium text-gray-500 dark:text-gray-400 mx-auto p-3"
+    >
+      Home
+    </RouterLink>
+  </FwbCard>
 
-    <FwbCard variant="image" class="mx-auto mt-10">
-      <FwbAlert class="text-md">
-        <FwbA
-          href="https://www.avs4you.com/blog/change-text-to-speech-language-voice/"
-          class="font-bold"
-          target="_blank"
-        >
-          External link
-        </FwbA>
-        <FwbP>Install a new Text-to-Speech language on Windows 7, 10 and 11</FwbP>
-      </FwbAlert>
-    </FwbCard>
-  </main>
+  <FwbCard variant="image" class="mx-auto mt-10">
+    <FwbAlert class="text-md">
+      <FwbA
+        href="https://www.avs4you.com/blog/change-text-to-speech-language-voice/"
+        class="font-bold"
+        target="_blank"
+      >
+        External link
+      </FwbA>
+      <FwbP>Install a new Text-to-Speech language on Windows 7, 10 and 11</FwbP>
+    </FwbAlert>
+  </FwbCard>
 </template>
