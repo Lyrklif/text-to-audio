e3c3c1e046aff92ea07c7ace516b5a54fa331624
Alexandr Kochurov <b2b4ly@gmail.com>
2024-10-16 06:54:11 +0400

    feat: change max text length

--- src/constatns/voice.ts
+++ src/constatns/voice.ts
@@ -1,7 +1,7 @@
 export const SPEED_DEFAULT: number = 1.0
 export const PITCH_DEFAULT: number = 1.0
 
-export const MAX_TEXT_LENGTH: number = 200
+export const MAX_TEXT_LENGTH: number = 400
 
 export const DEFAULT_TEXT: string = `Online Text to Audio converts text in any languages into audio. 
 You can download audio in MP3 format`
